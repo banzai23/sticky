f121c95156b2e7f8edde872e6058d2af56713a8c
Michael McQueary <mcq.michael@gmail.com>
2022-01-27 02:02:38 -0700

    Proper configuration for Typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { SimpleGrid } from '@mantine/core';
+
+export function App() {
+    return (
+        <SimpleGrid cols={3}>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+            <div>7</div>
+            <div>8</div>
+            <div>9</div>
+        </SimpleGrid>
+    );
+}
