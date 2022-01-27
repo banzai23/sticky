d763ec984efecd52bf7d0d45dbd5ba55bc1f1f21
Michael McQueary <mcq.michael@gmail.com>
2022-01-27 00:32:13 -0700

    Added Typescript-React with Mantine for Frontend

--- src/main/kotlin/com/mcqueary/plugins/Serialization.kt
+++ src/main/kotlin/com/mcqueary/plugins/Serialization.kt
@@ -6,12 +6,10 @@ import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.*
 import kotlinx.serialization.json.*
 import java.sql.DriverManager
-import java.sql.Timestamp
 
 val HOST = System.getenv("HOST")
 val USERNAME = System.getenv("USERNAME")
